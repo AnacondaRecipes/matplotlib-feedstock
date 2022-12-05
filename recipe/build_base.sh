e861f8a94a8f1ec17a75b634ad177043df93da4c
Eric Lundby <Eric.Lundby@gmail.com>
2022-12-05 16:15:50 -0700

    DONT MERGE: Try manually downloading the file.

--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -19,6 +19,9 @@ sed -i.bak "s|/usr/local|${PREFIX}|" setupext.py
 
 export MPLSETUPCFG=conda_mpl_config.cfg
 export XDG_CACHE_HOME=${BUILD_PREFIX}/matplotlib.cache/
+mkdir build
+curl http://www.qhull.org/download/qhull-2020-src-8.0.2.tgz --output ./build/qhull-2020-src-8.0.2.tgz
+tar zxvf ./build/qhull-2020-src-8.0.2.tgz -C ./build
 
 if [[ $(uname) == "Darwin" ]]; then
   export CFLAGS="${CFLAGS} -Wno-unused-command-line-argument"
