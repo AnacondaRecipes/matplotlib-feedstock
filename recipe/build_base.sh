d15962f55caa5ef7f6fe5f4ede9959b6eb8ec88a
Anton Kabanovs <akabanovs@anaconda.com>
2023-02-27 11:45:34 +0000

    matplotlib:
     - removed zlib from dependencies as there is no direct one.
     - numpy in host should be pinned to the version specified in global cbc.
     - Updated build_base.sh script to avoid build problems on linux archs, s390x specifically.

--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -24,4 +24,18 @@ if [[ $(uname) == "Darwin" ]]; then
   export CXXFLAGS="${CXXFLAGS} -Wno-unused-command-line-argument"
 fi
 
-$PYTHON -m pip install . --no-deps -vv
+# System's gcc compiler is randomly picked on linux-s390x,
+# rather than the conda's one, so we need to force it to use the correct one.
+# Probably worth doing on all linux based archs.
+# Also, it builds some parts of numpy when creating the wheel. Compilation 
+# crashes on s390x arch, unless the following optimisation flags are present.
+if [[ $target_platform == linux-* ]]; then
+  ln -f -s ${CC} gcc
+  export PATH=${PWD}:$PATH
+  if [[ $target_platform == "linux-s390x" ]]; then
+    export CFLAGS="${CFLAGS} -mno-vx"
+    export CXXFLAGS="${CXXFLAGS} -mno-vx"
+  fi
+fi
+
+$PYTHON -m pip install . --no-deps -vvv
