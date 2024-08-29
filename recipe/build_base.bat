81a39e7c5390045fdd4070e314f824ba9e3769c7
Serhii Kupriienko <skupriienko@anaconda.com>
2024-08-29 17:10:32 +0300

    Try to build from wheels

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -9,5 +9,10 @@ type builddir\meson-logs\meson-log.txt
 %PYTHON% -m build --wheel ^
          --no-isolation --skip-dependency-check -Cbuilddir=builddir -Ccompile-args=-v
 if errorlevel 1 exit 1
-%PYTHON% -m pip install  dist\matplotlib*.whl
-if errorlevel 1 exit 1
+
+:: `pip install dist\numpy*.whl` does not work on windows,
+:: so use a loop; there's only one wheel in dist/ anyway
+for /f %%f in ('dir /b /S .\dist') do (
+    pip install %%f
+    if %ERRORLEVEL% neq 0 exit 1
+)
