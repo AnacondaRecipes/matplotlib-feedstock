65025bbdf6810ebab2445153888c7f5e9b5cae21
Serhii Kupriienko <skupriienko@anaconda.com>
2024-08-29 17:59:06 +0300

    Add config-settings

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -1,4 +1,8 @@
 @echo on
 
-%PYTHON% -m pip install . --no-deps --no-build-isolation -vv
+set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig"
+
+%PYTHON% -m pip install . --no-deps --no-build-isolation -vv ^
+    --config-settings=setup-args="-Dsystem-freetype=true" ^
+    --config-settings=setup-args="-Dsystem-qhull=true"
 if errorlevel 1 exit 1
--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -2,5 +2,9 @@
 set -ex
 
 export AR=$GCC_AR
+export PKG_CONFIG_PATH="${BUILD_PREFIX}/bin/pkg-config:${BUILD_PREFIX}/lib/pkgconfig:${PREFIX}/bin/pkg-config:${PREFIX}/lib/pkg-config"
 
-$PYTHON -m pip install . --no-deps --no-build-isolation -vv
+$PYTHON -m pip install . -vv \
+    --no-deps --no-build-isolation \
+    --config-settings=setup-args="-Dsystem-freetype=true" \
+    --config-settings=setup-args="-Dsystem-qhull=true"
