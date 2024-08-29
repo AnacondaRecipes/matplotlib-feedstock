293e9cb116f2cfc0137e3f6402b0a691f424bb81
Serhii Kupriienko <skupriienko@anaconda.com>
2024-08-29 17:36:37 +0300

    Use pip install on win

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -1,18 +1,4 @@
 @echo on
 
-set "MESON_ARGS=%MESON_ARGS% --buildtype=release --prefix=%LIBRARY_PREFIX% --pkg-config-path=%LIBRARY_LIB%\pkgconfig -Dlibdir=lib -Dsystem-freetype=true -Dsystem-qhull=true"
-
-mkdir builddir
+%PYTHON% -m pip install . --no-deps --no-build-isolation -vv
 if errorlevel 1 exit 1
-%PYTHON% -m mesonbuild.mesonmain setup builddir %MESON_ARGS%
-type builddir\meson-logs\meson-log.txt
-%PYTHON% -m build --wheel ^
-         --no-isolation --skip-dependency-check -Cbuilddir=builddir -Ccompile-args=-v
-if errorlevel 1 exit 1
-
-:: `pip install dist\numpy*.whl` does not work on windows,
-:: so use a loop; there's only one wheel in dist/ anyway
-for /f %%f in ('dir /b /S .\dist') do (
-    pip install %%f
-    if %ERRORLEVEL% neq 0 exit 1
-)
