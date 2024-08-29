37f414e8e6b8b8f9eaf7952ca8433550f8a7d5c7
Charles Bousseau <cbousseau@anaconda.com>
2024-08-29 14:01:42 -0400

    remove mpl_sample_data, adapt pip install

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -1,7 +1,6 @@
 @echo on
 
 set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig"
-mkdir builddir
 "%PYTHON%" -m build --wheel --no-isolation --skip-dependency-check ^
     -Cbuilddir=builddir ^
     -Csetup-args=-Dsystem-freetype=true ^
@@ -11,9 +10,4 @@ if errorlevel 1 (
   exit /b 1
 )
 
-:: `pip install dist\numpy*.whl` does not work on windows,
-:: so use a loop; there's only one wheel in dist/ anyway
-for /f %%f in ('dir /b /S .\dist') do (
-    pip install %%f
-    if %ERRORLEVEL% neq 0 exit 1
-)
+%PYTHON% -m pip install --no-deps --no-build-isolation --no-index --find-links dist matplotlib
--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -2,10 +2,9 @@
 
 set -ex
 
-mkdir builddir
 $PYTHON -m build --wheel --no-isolation --skip-dependency-check \
     -Cbuilddir=builddir \
     -Csetup-args=-Dsystem-freetype=true \
     -Csetup-args=-Dsystem-qhull=false \
     || (cat builddir/meson-logs/meson-log.txt && exit 1)
-$PYTHON -m pip install dist/matplotlib*.whl
+$PYTHON -m pip install --no-deps --no-build-isolation --no-index --find-links dist matplotlib
