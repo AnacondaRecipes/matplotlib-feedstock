90fb2167b8ed1b6a8137135b1821a2c2052480ed
Serhii Kupriienko <skupriienko@anaconda.com>
2024-08-30 10:39:34 +0300

    Fix build scripts, clean up the recipe

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -1,8 +1,14 @@
 @echo on
 
 set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig"
+"%PYTHON%" -m build --wheel --no-isolation --skip-dependency-check ^
+    -Cbuilddir=builddir ^
+    -Csetup-args=-Dsystem-freetype=true ^
+    -Csetup-args=-Dsystem-qhull=false
+if errorlevel 1 (
+  type builddir\meson-logs\meson-log.txt
+  exit /b 1
+)
 
-%PYTHON% -m pip install . --no-deps --no-build-isolation -vv ^
-    --config-settings=setup-args="-Dsystem-freetype=true" ^
-    --config-settings=setup-args="-Dsystem-qhull=true"
+%PYTHON% -m pip install --no-deps --no-build-isolation --no-index --find-links dist matplotlib
 if errorlevel 1 exit 1
--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -2,9 +2,10 @@
 set -ex
 
 export AR=$GCC_AR
-export PKG_CONFIG_PATH="${BUILD_PREFIX}/bin/pkg-config:${BUILD_PREFIX}/lib/pkgconfig:${PREFIX}/bin/pkg-config:${PREFIX}/lib/pkg-config"
 
-$PYTHON -m pip install . -vv \
-    --no-deps --no-build-isolation \
-    --config-settings=setup-args="-Dsystem-freetype=true" \
-    --config-settings=setup-args="-Dsystem-qhull=true"
+$PYTHON -m build --wheel --no-isolation --skip-dependency-check \
+    -Cbuilddir=builddir \
+    -Csetup-args=-Dsystem-freetype=true \
+    -Csetup-args=-Dsystem-qhull=false \
+    || (cat builddir/meson-logs/meson-log.txt && exit 1)
+$PYTHON -m pip install --no-deps --no-build-isolation --no-index --find-links dist matplotlib
